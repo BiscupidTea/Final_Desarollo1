34f3cb9bfe67c2f004473c496b1b7b8812653bbd
Enzo <coletta.enzo@yahoo.com.ar>
2023-01-04 17:07:55 -0300

    Create Entity

--- src/objects/Entity.cpp
+++ src/objects/Entity.cpp
@@ -1,1 +1,43 @@
 #include "Entity.h"
+
+Entity::Entity(Vector2 position, int width, int height)
+{
+	this->position = position;
+	this->width = width;
+	this->height = height;
+}
+
+Entity::~Entity()
+{
+
+}
+
+void Entity::SetPosition(Vector2 position)
+{
+	this->position = position;
+}
+
+Vector2 Entity::GetPosition()
+{
+	return position;
+}
+
+int Entity::GetX()
+{
+	return position.x;
+}
+
+int Entity::GetY()
+{
+	return position.y;
+}
+
+int Entity::GetWidth()
+{
+	return this->width;
+}
+
+int Entity::GetHeight()
+{
+	return this->height;
+}
--- src/objects/Entity.h
+++ src/objects/Entity.h
@@ -1,5 +1,25 @@
 #pragma once
+#include "raylib.h"
+
 class Entity
 {
+protected:
+	Vector2 position;
+	int width;
+	int height;
+
+public:
+	Entity(Vector2 position, int width, int height);
+	virtual ~Entity();
+
+	virtual void Draw() = 0;
+
+	void SetPosition(Vector2 position);
+	Vector2 GetPosition();
+	int GetX();
+	int GetY();
+	int GetWidth();
+	int GetHeight();
 };
+
 
