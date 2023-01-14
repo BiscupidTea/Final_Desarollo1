b48851e3f8316b5dc1d6efc92832380d3aaf1997
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-01-13 23:34:41 -0300

    add Obstacle cpp and h

---
+++ src/objects/Obstacle.cpp
@@ -0,0 +1,24 @@
+#include "Obstacle.h"
+
+Obstacle::Obstacle(Vector2 position, Vector2 velocity, int width, int height) : Entity(position, velocity, width, height)
+{
+	this->position = position;
+	this->velocity = velocity;
+	this->width = width;
+	this->height = height;
+}
+
+Obstacle::~Obstacle()
+{
+
+}
+
+void Obstacle::Draw()
+{
+	DrawRectangle(static_cast<int>(GetX()), static_cast<int>(GetY()), GetWidth(), GetHeight(), RED);
+}
+
+void Obstacle::Movement()
+{
+
+}
---
+++ src/objects/Obstacle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "tools/tools.h"
+
+#include "Entity.h"
+
+class Obstacle : public Entity
+{
+private:
+
+
+public:
+	Obstacle(Vector2 position, Vector2 velocity, int width, int height);
+	~Obstacle();
+
+	void Draw();
+
+	void Movement();
+};
