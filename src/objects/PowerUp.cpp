0258931f4e971e8080399f25bfe0772b43676ca4
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-02-02 16:49:12 -0300

    add power up class

--- src/objects/PowerUp.cpp
+++ src/objects/PowerUp.cpp
@@ -1,1 +1,47 @@
 #include "PowerUp.h"
+
+PowerUp::PowerUp(Vector2 position, Vector2 velocity, int width, int height) : Entity(position, velocity, width, height)
+{
+	this->position = position;
+	this->velocity = velocity;
+	this->width = width;
+	this->height = height;
+	this->picked = false;
+
+	this->spawnItem = new Timer(30);
+}
+
+PowerUp::~PowerUp()
+{
+
+}
+
+void PowerUp::Draw()
+{
+	DrawRectangle(static_cast<int>(GetX()), static_cast<int>(GetY()), width, height, SKYBLUE);
+}
+
+void PowerUp::Movement()
+{
+	position.x -= velocity.x * GetFrameTime();
+}
+
+bool PowerUp::OutOfLimits()
+{
+	if (GetX() + width < 0)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void PowerUp::SetPicked(bool setter)
+{
+	picked = setter;
+}
+
+bool PowerUp::IsPicked()
+{
+	return picked;
+}
--- src/objects/PowerUp.h
+++ src/objects/PowerUp.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "Entity.h"
-class PowerUp :
-    public Entity
+
+#include "tools/Timer.h"
+
+class PowerUp :	public Entity
 {
+private:
+	bool picked;
+	Timer* spawnItem;
+
+public:
+	PowerUp(Vector2 position, Vector2 velocity, int width, int height);
+	~PowerUp();
+
+	void Draw();
+
+	void Movement();
+
+	bool OutOfLimits();
+
+	void SetPicked(bool setter);
+	bool IsPicked();
 };
 
